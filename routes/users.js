08cf6c23fdebeb91b20e0c14246922ccd3da0ad6
hsimao <e087754958@gmail.com>
2021-06-29 15:47:11 +0800

    feat: get user list api

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 // 用戶管理
 const router = require('koa-router')()
 const User = require('./../models/userSchema')
-const utils = require('./../utils/util')
+const utils = require('../utils')
 const { createToken } = require('./../utils/jwt')
 
 router.prefix('/users')
@@ -34,6 +34,39 @@ router.post('/login', async (ctx, next) => {
     }
   } catch (error) {
     ctx.body = utils.fail(error.msg)
+  }
+})
+
+// 用戶列表
+router.get('/list', async (ctx) => {
+  const { userId, userName, state } = ctx.request.query
+  const { page, skipIndex } = utils.pager(ctx.request.query)
+
+  const params = {}
+
+  if (userId) params.userId = userId
+  if (userName) params.userName = userName
+  if (state && state !== '0') params.state = state
+
+  try {
+    const query = User.find(params, {
+      _id: 0,
+    })
+
+    // 分頁, 從 skipIndex 開始查詢
+    const list = await query.skip(skipIndex).limit(page.pageSize)
+    // 總筆數
+    const total = await User.countDocuments(params)
+
+    ctx.body = utils.success({
+      page: {
+        ...page,
+        total,
+      },
+      list,
+    })
+  } catch (error) {
+    ctx.body = utils.fail(`查詢異常: ${error.stack}`)
   }
 })
 
